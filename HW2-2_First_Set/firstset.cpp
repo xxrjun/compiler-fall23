de0401ce4d76baf0b4ffdecbb9aa9bb1ce5bd1af
rjun <rjun0729@gmail.com>
2023-11-21 14:22:38 +0800

    Refactor split function and computeFirstSet function

--- HW2-2_First_Set/firstset.cpp
+++ HW2-2_First_Set/firstset.cpp
@@ -6,24 +6,39 @@
 
 using namespace std;
 
-// Function to split a string by a delimiter and return a vector of tokens
+/**
+ * @brief 將字符串根據指定分隔符拆分成子字符串
+ *
+ * @param str 要拆分的字符串
+ * @param delimiter 分隔符
+ * @return vector<string> 包含子字符串的向量
+ */
 vector<string> split(const string &str, char delimiter)
 {
     vector<string> tokens;
     stringstream ss(str);
     string token;
+
+    // 迴圈逐步讀取: 使用 getline 在每次迭代中從 ss 讀取直到遇到分隔符 delimiter 的字符串
     while (getline(ss, token, delimiter))
     {
-        tokens.push_back(token);
+        tokens.push_back(token); // 將拆分後的子字符串添加到向量中
     }
     return tokens;
 }
 
-// Function to compute First Set for a given symbol (terminal or nonterminal)
+/**
+ * @brief 計算給定符號的 First Set
+ *
+ * @param symbol 要計算 First Set 的符號
+ * @param rules 包含語法規則的映射
+ * @param firstSets 用於存儲已計算 First Sets 的映射
+ * @return 給定符號的 First Set
+ */
 set<char> computeFirstSet(const string &symbol, map<char, vector<string>> &rules, map<char, set<char>> &firstSets)
 {
     // 如果已經計算過此符號的 First Set，則直接返回已儲存的結果
-    // 可以避免遞迴產生規則或交叉參照的情況下無限遞迴 (雖然題目說不會有前者情況)
+    // 這樣可以提高效率，避免重複計算
     if (firstSets.find(symbol[0]) != firstSets.end())
     {
         return firstSets[symbol[0]];
@@ -31,74 +46,85 @@ set<char> computeFirstSet(const string &symbol, map<char, vector<string>> &rules
 
     set<char> firstSet;
 
-    // 若符號是終端符號、EOF 或 epsilon，則其 First Set 就是其本身
+    // 如果符號是終端符號、EOF 或 epsilon (分別用小寫字母、'$' 和 ';' 表示)
+    // 則其 First Set 就是其本身
     if (islower(symbol[0]) || symbol[0] == ';' || symbol[0] == '$')
     {
         firstSet.insert(symbol[0]);
         return firstSet;
     }
 
-    // 若符號是非終端符號，則基於其產生規則計算 First Set
+    // 如果符號是非終端符號，則需要基於其產生規則來計算 First Set
     for (const auto &production : rules[symbol[0]])
     {
-        // 檢查該產生規則是否所有符號均導向 epsilon
+        // 標記用於確定是否所有符號均導向 epsilon (空字串)
         bool allLeadToEpsilon = true;
 
+        // 遍歷產生規則中的每個符號
         for (const char &ch : production)
         {
-            // 遞迴計算當前符號的 First Set
+            // 遞迴調用來計算當前符號的 First Set
             set<char> subFirstSet = computeFirstSet(string(1, ch), rules, firstSets);
 
-            // 檢查當前符號是否導向 epsilon
+            // 判斷當前符號的 First Set 是否包含 epsilon
             allLeadToEpsilon = allLeadToEpsilon && (subFirstSet.find(';') != subFirstSet.end());
 
-            // 如果當前符號不導向 epsilon，則將除 epsilon 外的符號加入 firstSet
+            // 從子 First Set 中移除 epsilon
             subFirstSet.erase(';');
+            // 將子 First Set 中的元素加入到當前的 First Set
             firstSet.insert(subFirstSet.begin(), subFirstSet.end());
 
-            // 如果當前符號不導向 epsilon，則停止處理這條產生規則
+            // 如果當前符號的 First Set 不包含 epsilon，則停止處理這條產生規則
             if (!allLeadToEpsilon)
             {
                 break;
             }
         }
 
-        // 如果該產生規則所有符號均導向 epsilon，則將 epsilon 加入 firstSet
+        // 如果產生規則中的所有符號的 First Set 均包含 epsilon，則將 epsilon 加入到 First Set
         if (allLeadToEpsilon)
         {
             firstSet.insert(';');
         }
     }
 
-    // 儲存並返回計算出的 First Set
+    // 儲存計算出的 First Set 以供後續使用
     firstSets[symbol[0]] = firstSet;
     return firstSet;
 }
 
+// 主函數
 int main()
 {
     string line;
 
-    // 使用 map 儲存文法規則以及firstSet，key 會根據 ASCII 編碼自動排序
+    // 規則和 First Sets 的映射
     map<char, vector<string>> rules;
     map<char, set<char>> firstSets;
 
-    // Read grammar rules
+    // 從標準輸入讀取語法規則
     while (getline(cin, line) && line != "END_OF_GRAMMAR")
     {
+        // 使用 split 函數將讀取的行按空格拆分為 tokens
         vector<string> tokens = split(line, ' ');
+
+        // 取出 tokens 第一個元素的第一個字符，作為非終端符號
         char nonterminal = tokens[0][0];
+
+        // 使用 split 函數將 tokens 第二個元素按 '|' 拆分為 productions
         vector<string> productions = split(tokens[1], '|');
+
+        // 將非終端符號和其對應的產生規則存入映射 rules 中
         rules[nonterminal] = productions;
     }
 
-    // Compute First Sets for each nonterminal
+    // 為每個非終端符號計算 First Set
     for (const auto &rule : rules)
     {
         firstSets[rule.first] = computeFirstSet(string(1, rule.first), rules, firstSets);
     }
 
-    // Output the First Sets
+    // 輸出 First Sets
     for (const auto &firstSet : firstSets)
     {
         cout << firstSet.first << " ";
